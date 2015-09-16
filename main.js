043b6931c3f8e3752455316ef9e829c88752ff38
bbouley <b.bouley@hotmail.co.uk>
2015-09-16 16:51:40 -0600

    added forms show and hide for comments

--- main.js
+++ main.js
@@ -1,21 +1,56 @@
 var app = angular.module('controllerApp', ['ngSanitize']);
 
-var formControllerFunction = function($scope){
+var mainControllerFunction = function($scope){
   $scope.posts = [];
   $scope.showForm = false;
   $scope.newPost = {
     postTitle : '',
     postAuthor : '',
     postDescription : '',
-    postPhoto : ''
+    postPhoto : '',
   };
   $scope.savePost = function(title, author, description, photo){
-    var post = {title: title.$modelValue, author: author.$modelValue, description: description.$modelValue, photoURL: photo.$modelValue };
+    var post = {
+      title: title.$modelValue,
+      author: author.$modelValue,
+      description: description.$modelValue,
+      photoURL: photo.$modelValue,
+      date: Date.now(),
+      votes: 0,
+      comments : [],
+      showComments: false,
+      showCommentForm: false
+    };
     $scope.posts.push(post);
     $scope.newPost = {};
     $scope.postForm.$setPristine();
     $scope.showForm = false;
     console.log($scope.posts);
+  };
+  $scope.postUp = function(){
+    this.post.votes += 1;
+  };
+  $scope.postDown = function(){
+    this.post.votes -= 1;
+  };
+
+  $scope.showCommentFormFunction = function(){
+    this.post.showCommentForm = true;
+  };
+  $scope.newComment = function(title, mainText){
+    var comment = {
+      title : title.$modelValue,
+      mainText : mainText.$modelValue,
+    };
+    this.post.comments.push(comment);
+    console.log(this.post.comments);
+    this.post.showCommentForm = false;
+  };
+  $scope.showCommentsSection = function(){
+    this.post.showComments = true;
+  };
+  $scope.hideCommentsSection= function(){
+    this.post.showComments = false;
   };
 };
 
@@ -23,6 +58,6 @@ var postControllerFunction = function($scope){
   $scope.test = 'test';
 };
 
-app.controller('postController', ['$scope', postControllerFunction]);
+// app.controller('postController', ['$scope', postControllerFunction]);
 
-app.controller('formController', ['$scope', formControllerFunction]);
+app.controller('mainController', ['$scope', mainControllerFunction]);
